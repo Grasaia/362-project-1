1b17505e47ea5f6f7a0a01bab55c963a5e949eb3
Grasaia <sdugger@hawk.iit.edu>
2024-02-29 17:58:01 -0600

    added constants for name email and experience

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('signup-form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const experience = document.getElementById('experience').value;
+        
     }
 
 }
