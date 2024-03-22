54ebd98dd4c0555b0dbb8489e9723dac434f1725
Grasaia <sdugger@hawk.iit.edu>
2024-03-21 19:52:34 -0500

    added function in js to display a message once info is sent

--- js/validation.js
+++ js/validation.js
@@ -5,5 +5,15 @@ document.getElementById('signup-form').addEventListener('submit', function(event
     if (!emailRegex.test(email)) {
         event.preventDefault();
         alert('Please enter a valid email address.');
+    } else {
+        event.preventDefault();
+        document.getElementById('signup-form').reset(); // Reset the form
+        displaySuccessMessage();
     }
 });
+
+function displaySuccessMessage() {
+    var messageElement = document.createElement('p');
+    messageElement.textContent = "Hooray your information has been submitted";
+    document.getElementById('signup-form').appendChild(messageElement);
+}
